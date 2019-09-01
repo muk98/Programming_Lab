19fe8ca626b9f476d8dac969d091a14f5fe2b522
muk98 <vmukul044@gmail.com>
2019-09-01 14:27:34 +0530

    Bug Fixes

--- Assignment-1/Part-2/Manufacturing.java
+++ Assignment-1/Part-2/Manufacturing.java
@@ -43,7 +43,8 @@ class PackagingUnitThread implements Runnable {
     UnfinishedTray unfinishedTray;
     int processingTime;
     GoDown goDown;
-
+    int currBottleToDraw;
+    boolean empty;
     PackagingUnitThread(Phaser p, String name,Bottles bottle,Tray sealingTray,Tray B1Tray,Tray B2Tray
                             ,UnfinishedTray unfinishedTray,GoDown goDown)
 	{ 
@@ -57,7 +58,8 @@ class PackagingUnitThread implements Runnable {
         this.unfinishedTray = unfinishedTray;
         this.processingTime = 0;
         this.goDown = goDown;
-
+        this.currBottleToDraw = 2;
+        this.empty=true;
 		phsr.register(); 
 		new Thread(this).start(); 
 	} 
@@ -72,49 +74,52 @@ class PackagingUnitThread implements Runnable {
             
             // System.out.println("Currently Processing Bottle :" + Integer.toString(this.currentBottle.id));
             this.processingTime++;
-            if(this.currentBottle != null && this.processingTime<2){
+            if(this.empty==false && this.processingTime<3){
                 System.out.println("Packaging Unit: Currently Processing Bottle :" + Integer.toString(this.currentBottle.id));
             }
             else{
-                boolean flag=false;
-                if(this.processingTime>=2){
+                if(this.processingTime>=3 && this.empty==false){
                     this.currentBottle.packagingStatus =true;
+                    System.out.println("Packaging DONE!!" + Integer.toString(this.currentBottle.type) + " "+  Integer.toString(this.currentBottle.id));
                     if(this.currentBottle.sealingStatus == true){
                         if(this.currentBottle.type == 1){
                             this.goDown.totalB1++;
                         }
                         else this.goDown.totalB2++;
-                        flag=true;
+                        this.empty=true;
                         System.out.println("DONE!!" + Integer.toString(this.currentBottle.type));
                     }
                     else if(this.currentBottle.type == 1 && this.sealingTray.currentBottlesNum < this.sealingTray.size){
                         int curr = this.sealingTray.currentBottlesNum;
                         this.sealingTray.bottlesId[curr] = this.currentBottle;
                         this.sealingTray.currentBottlesNum++;
-                        flag=true;
+                        this.empty=true;
                     }
                 }
-
-                if(flag==true || this.currentBottle==null){
+                else if(this.empty==true){
                     if(this.B1PackagingTray.currentBottlesNum==0 && this.B2PackagingTray.currentBottlesNum==0){
                         if(unfinishedTray.currBottleToDraw ==-1){
                             System.out.println("No bottles left to Draw !!");
                         }
                         else{
-                            Bottles bottle = new Bottles(unfinishedTray.id++, unfinishedTray.currBottleToDraw);
+                            Bottles bottle = new Bottles(unfinishedTray.id++, this.currBottleToDraw);
                             this.currentBottle = bottle;
-                            System.out.println("Bottle drawn by Packaging Unit from Unfinished Tray !! of type:" + Integer.toString(this.currentBottle.type));
-                            if(unfinishedTray.currBottleToDraw==1) {
-                                if(unfinishedTray.totalB2>0) unfinishedTray.currBottleToDraw=2;
+                            System.out.println("Bottle drawn by Packaging Unit from Unfinished Tray !! of type:" + Integer.toString(this.currentBottle.id));
+                            if(this.currentBottle.type==1) {
                                 unfinishedTray.totalB1--;
+                                if(unfinishedTray.totalB2>0) this.currBottleToDraw=2;
+                                else if(unfinishedTray.totalB1>0) this.currBottleToDraw=1;
                             }
                             else{
-                                if(unfinishedTray.totalB1>0) unfinishedTray.currBottleToDraw=1;
                                 unfinishedTray.totalB2--;
+                                if(unfinishedTray.totalB1>0) this.currBottleToDraw=1;
+                                else if(unfinishedTray.totalB2>0) this.currBottleToDraw=2;
                             }
+                            
                             if(unfinishedTray.totalB1+unfinishedTray.totalB2<=0) {
                                 unfinishedTray.currBottleToDraw = -1; 
                             }
+                            this.empty=false;
                         }
                     }
                     else{
@@ -157,9 +162,10 @@ class PackagingUnitThread implements Runnable {
                             this.B2PackagingTray.bottlesId[2] = null;
                             this.B2PackagingTray.currentBottlesNum--;
                         }
+                        this.empty=false;
                         System.out.println("Bottle drawn by Packaging Unit from Tray !! of type:" + Integer.toString(this.currentBottle.type));
                     }
-                    this.processingTime = 0;
+                    this.processingTime = 1;
                 }
             }
 
@@ -184,7 +190,10 @@ class SealingUnitThread implements Runnable {
     Tray B2PackagingTray;
     UnfinishedTray unfinishedTray;
     int processingTime;
+    boolean empty;
+
     GoDown goDown;
+    int currBottleToDraw;
 
     SealingUnitThread(Phaser p, String name,Bottles bottle,Tray sealingTray,Tray B1Tray,Tray B2Tray
                             ,UnfinishedTray unfinishedTray,GoDown goDown)
@@ -198,6 +207,8 @@ class SealingUnitThread implements Runnable {
         this.unfinishedTray = unfinishedTray;
         this.processingTime = 0;
         this.goDown = goDown;
+        this.currBottleToDraw =  1;
+        this.empty=true;
 
 		phsr.register(); 
 		new Thread(this).start(); 
@@ -212,55 +223,58 @@ class SealingUnitThread implements Runnable {
 							+ phsr.getPhase()); 
             
             this.processingTime++;
-            if(this.currentBottle != null && this.processingTime<2){
+            if(this.empty==false && this.processingTime<2){
                 System.out.println("Sealing Unit: Currently Processing Bottle :" + Integer.toString(this.currentBottle.id));
             }
             else{
-                boolean flag = false;
-                if(this.processingTime>=2){
+                if(this.processingTime>=2 && this.empty==false){
+                    System.out.println("Sealing DONE!!" + Integer.toString(this.currentBottle.type) + " "+  Integer.toString(this.currentBottle.id));
                     this.currentBottle.sealingStatus= true;
                     if(this.currentBottle.packagingStatus == true){
                         if(this.currentBottle.type == 1){
                             this.goDown.totalB1++;
                         }
                         else this.goDown.totalB2++;
-                        flag=true;
+                        this.empty = true;
                         System.out.println("DONE!!" + Integer.toString(this.currentBottle.type));
                     }
                     else if(this.currentBottle.type == 1 && this.B1PackagingTray.currentBottlesNum < this.B1PackagingTray.size){
                         int curr = this.B1PackagingTray.currentBottlesNum;
                         this.B1PackagingTray.bottlesId[curr] = this.currentBottle;
                         this.B1PackagingTray.currentBottlesNum++;
-                        flag=true;
+                        this.empty = true;
                     }
                     else if(this.currentBottle.type == 2 && this.B2PackagingTray.currentBottlesNum < this.B2PackagingTray.size){
                         int curr = this.B2PackagingTray.currentBottlesNum;
                         this.B2PackagingTray.bottlesId[curr] = this.currentBottle;
                         this.B2PackagingTray.currentBottlesNum++;
-                        flag=true;
+                        this.empty = true;
                     }
                 }
-
-                if(flag==true || this.currentBottle==null){
+                else if(this.empty ==true){
                     if(this.sealingTray.currentBottlesNum==0){
                         if(unfinishedTray.currBottleToDraw ==-1){
                             System.out.println("No bottles left to Draw !!");
                         }
                         else{
-                            Bottles bottle = new Bottles(unfinishedTray.id++, unfinishedTray.currBottleToDraw);
+                            Bottles bottle = new Bottles(unfinishedTray.id++, this.currBottleToDraw);
                             this.currentBottle = bottle;
-                            if(unfinishedTray.currBottleToDraw==1) {
-                                if(unfinishedTray.totalB2>0) unfinishedTray.currBottleToDraw=2;
+                            if(this.currentBottle.type==1) {
                                 unfinishedTray.totalB1--;
+                                if(unfinishedTray.totalB2>0) this.currBottleToDraw=2;
+                                else if(unfinishedTray.totalB1>0) this.currBottleToDraw=1;
                             }
                             else{
-                                if(unfinishedTray.totalB1>0) unfinishedTray.currBottleToDraw=1;
                                 unfinishedTray.totalB2--;
+                                if(unfinishedTray.totalB1>0) this.currBottleToDraw=1;
+                                else if(unfinishedTray.totalB2>0) this.currBottleToDraw=2;
                             }
+
                             if(unfinishedTray.totalB1+unfinishedTray.totalB2<=0) {
                                 unfinishedTray.currBottleToDraw = -1; 
                             }
-                            System.out.println("Bottle drawn by Sealing Unit from Unfinished Tray !! of type:" + Integer.toString(this.currentBottle.type));
+                            this.empty=false;
+                            System.out.println("Bottle drawn by Sealing Unit from Unfinished Tray !! of type:" + Integer.toString(this.currentBottle.id));
                         }
                     }
                     else{
@@ -268,10 +282,11 @@ class SealingUnitThread implements Runnable {
                         this.sealingTray.bottlesId[0] = this.sealingTray.bottlesId[1];
                         this.sealingTray.bottlesId[1] = null;
                         this.sealingTray.currentBottlesNum--;
+                        this.empty=false;
                         System.out.println("Bottle drawn by Sealing Unit from Tray !! of type:" + Integer.toString(this.currentBottle.type));
   
                     }
-                    this.processingTime = 0;
+                    this.processingTime = 1;
                 }
             }
             
