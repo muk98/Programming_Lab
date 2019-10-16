b7051f28b309e010abd74f46f07bfa47e2636a56
muk98 <vmukul044@gmail.com>
2019-10-16 23:20:32 +0530

    Question2 done,3 started

--- Assignment-2/Part-2/ques2.pl
+++ Assignment-2/Part-2/ques2.pl
@@ -1,35 +1,62 @@
-item('starter',30,"yo").
-item('starter',10,"oo").
-item('main',40,"my").
-item('main',40,"mo").
-item('desert',40,"dy").
-item('desert',40,"do").
-starters(["yo","oo"]).
+item('starter',30,"Corn Tikki").
+item('starter',20,"Tomato Soup").
+item('starter',40,"Chilli Paneer").
+item('starter',40,"Crispy Chicken").
+item('starter',20,"Papadi Chaat").
+item('starter',20,"Cold Drink").
 
+item('main_dish',50,"Kadhai Paneer with Butter/Plain Naan").
+item('main_dish',40,"Veg Korma with Butter/Plain Naan").
+item('main_dish',50,"Murgh Lababdar with Butter/Plain Naan").
+item('main_dish',50,"Veg Dum Biryani with Dal Tadka").
+item('main_dish',40,"Steam Rice with Dal Tadka").
+
+item('dessert',20,"Ice-Cream").
+item('dessert',30,"Malayi Sandwich").
+item('dessert',10,"Rasmalai").
+starters(["Corn Tikki","Tomato Soup","Chilli Paneer","Crispy Chicken","Papadi Chaat","Cold Drink"]).
+main_dish(["Kadhai Paneer with Butter/Plain Naan","Veg Korma with Butter/Plain Naan","Murgh Lababdar with Butter/Plain Naan","Veg Dum Biryani with Dal Tadka","Steam Rice with Dal Tadka"]).
+dessert(["Ice-Cream","Malayi Sandwich","Rasmalai"]).
 
 check(1).
 notCheck(0).
 
 
-caseX:-check(X),notCheck(Y),notCheck(Z).
-caseY:- notCheck(X),check(Y),notCheck(Z).
-caseZ:- notCheck(X),notCheck(Y),check(Z).
-menu('diet',X,Y,Z):- caseX.
-menu('diet',X,Y,Z):- caseY.
-menu('diet',X,Y,Z):- caseZ.
+caseX(X,Y,Z):-check(X),notCheck(Y),notCheck(Z).
+caseY(X,Y,Z):- notCheck(X),check(Y),notCheck(Z).
+caseZ(X,Y,Z):- notCheck(X),notCheck(Y),check(Z).
+
+menu('diet',X,Y,Z):- caseX(X,Y,Z).
+menu('diet',X,Y,Z):- caseY(X,Y,Z).
+menu('diet',X,Y,Z):- caseZ(X,Y,Z).
 
 menu('hungry',X,Y,Z):- check(X),check(Y),check(Z).
 
 menu('not so hungry',X,Y,Z):- check(X),check(Y),notCheck(Z).
 menu('not so hungry',X,Y,Z):- notCheck(X),check(Y),check(Z).
 
+% findFood(Type,[A]) :- item(Type, _, A).
+% findFood(Type,T,S):- item(Type,_,A),,findFood(Type,T1),not(member(A,T1)),append(T1,[A,T).
+
+
 pick(H,Type,X,B):- item(Type,A,H),A=<X,B=A.
 
-printItems([H|T],Type,X,L):- pick(H,Type,X,B),write(H),nl,append(L,[H],L1),write(L1),nl,printItems(T,Type,X-B,L1).
+printItems([H|T],Type,X,L):- pick(H,Type,X,B),write("Items: "),write([H|L]),nl,append([H],L,L1),printItems1([H|T],Type,X-B,L1).
+printItems([_|T],Type,X,L):- printItems(T,Type,X,L).
+
+printItems1([H|T],Type,X,L):- pick(H,Type,X,B),append([H],L,L1),write("Items: "),write(L1),nl,printItems1([H|T],Type,X-B,L1).
+printItems1([_|T],Type,X,L):- printItems1(T,Type,X,L).
+
+print([H1|_],[H2|T2],Type1,Type2,X,L):- pick(H1,Type1,X,B1),printItems([H2|T2],Type2,X-B1,[H1|L]).
+print([H1|T1],[H2|T2],Type1,Type2,X,L):- pick(H1,Type1,X,B1),print([H1|T1],[H2|T2],Type1,Type2,X-B1,[H1|L]).
+print([_|T1],[H2|T2],Type1,Type2,X,L):- print(T1,[H2|T2],Type1,Type2,X,L).
 
 
-find_items('diet',X,Y,Z):- caseX,starters(T),printItems(T,'starter',40,[]). 
+find_items('diet',X,Y,Z):- caseX(X,Y,Z),starters(T), printItems(T,'starter',40,[]),!.
+find_items('diet',X,Y,Z):- caseY(X,Y,Z),main_dish(T), printItems(T,'main_dish',40,[]),!.
+find_items('diet',X,Y,Z):- caseZ(X,Y,Z),dessert(T), printItems(T,'dessert',40,[]),!.
 
-find_items('hungry',X,Y,Z,Items):- menu('hungry',X,Y,Z),item('starter',A,B),item('main',C,D),item('desert',E,F),atom_concat(B,", ",K),atom_concat(K,D,I),atom_concat(", ",F,G),atom_concat(I,G,Items).
+find_items('hungry',X,Y,Z):- menu('hungry',X,Y,Z),item('starter',_,B),item('main_dish',_,D),item('dessert',_,F),atom_concat(B,", ",K),atom_concat(K,D,I),atom_concat(", ",F,G),atom_concat(I,G,Items),write(Items).
 
-
+find_items('not so hungry',X,Y,Z):- check(X),check(Y),notCheck(Z),starters(T1),main_dish(T2),print(T1,T2,'starter','main_dish',80,[]),!.
+find_items('not so hungry',X,Y,Z):- notCheck(X),check(Y),check(Z),main_dish(T1),dessert(T2),print(T1,T2,'main_dish','dessert',80,[]),!.
---
+++ Assignment-2/Part-3/ques3.pl
@@ -0,0 +1,126 @@
+dist(g1,g5, 4).
+dist(g2,g5,6).
+dist(g3,g5,8).
+dist(g4,g5,9).
+dist(g1,g6,10).
+dist(g2,g6,9).
+dist(g3,g6,3).
+dist(g4,g6,5).
+dist(g5,g7,3).
+dist(g5,g10,4).
+dist(g5,g11,6).
+dist(g5,g12,7).
+dist(g5,g6,7).
+dist(g5,g8,9).
+dist(g6,g8,2).
+dist(g6,g12,3).
+dist(g6,g11,5).
+dist(g6,g10,9).
+dist(g6,g7,10).
+dist(g7,g10,2).
+dist(g7,g11,5).
+dist(g7,g12,7).
+dist(g7,g8,10).
+dist(g8,g9,3).
+dist(g8,g12,3).
+dist(g8,g11,4).
+dist(g8,g10,8).
+dist(g10,g15,5).
+dist(g10,g11,2).
+dist(g10,g12,5).
+dist(g11,g15,4).
+dist(g11,g13,5).
+dist(g11,g12,4).
+dist(g12,g13,7).
+dist(g12,g14,8).
+dist(g15,g13,3).
+dist(g13,g14,4).
+dist(g14,g17,5).
+dist(g14,g18,4).
+dist(g17,g18,8).
+dist(g5 ,g1, 4).
+dist(g5, g2,6).
+dist(g5, g3,8).
+dist(g5, g4,9).
+dist(g6, g1,10).
+dist(g6, g2,9).
+dist(g6, g3,3).
+dist(g6,g4,5).
+dist(g7, g5,3).
+dist(g10, g5,4).
+dist(g11, g5,6).
+dist(g12, g5,7).
+dist(g6, g5,7).
+dist(g8, g5,9).
+dist(g8,g6,2).
+dist(g12, g6,3).
+dist(g11, g6,5).
+dist(g10, g6,9).
+dist(g7, g6,10).
+dist(g10, g7,2).
+dist(g11, g7,5).
+dist(g12,g7,7).
+dist(g8, g7,10).
+dist(g9, g8,3).
+dist(g12, g8,3).
+dist(g11, g8,4).
+dist(g10, g8,8).
+dist(g15, g10,5).
+dist(g11,g10,2).
+dist(g12, g10,5).
+dist(g15, g11,4).
+dist(g13, g11,5).
+dist(g12, g11,4).
+dist(g13, g12,7).
+dist(g14,g12,8).
+dist(g13, g15,3).
+dist(g14, g13,4).
+dist(g17, g14,5).
+dist(g18, g14,4).
+dist(g18, g17,8).
+
+gates([g1,g2,g3,g4,g5,g6,g7,g8,g9,g10,g11,g12,g13,g14,g15,g16,g17,g18]).
+
+% start(g1).
+% start(g2).
+% start(g3).
+% start(g4).
+start([g1,g2,g3,g4]).
+end(g17).
+restr(g7).
+restr(g9).
+restr(g18).
+
+check(T) :- \+ T==[].
+
+valid([A,B|T]):- start(K),member(A,K),dist(A,B,_), valid1([B|T]).
+valid1([A,B|T]):- dist(A,B,_),check(T),valid1([B|T]),!.
+valid1([A,B|T]):- dist(A,B,_),\+ check(T),end(B).
+
+diff(X,H,L):- dist(X,H,_),L=H.
+diff(_,_,L):- L='na'.
+
+edges(_,[],[]).
+edges(X,[H|T],R):- diff(X,H,L),edges(X,T,R_tail),R=[L|R_tail],!.
+
+% path():- start(X),create_path(X,[X]).
+
+% create_path(X,L):- end(X),write("Path: "),write(L),nl,!.
+% create_path(X,L):- dist(X,Y,_),not(member(Y,L)),append(L,[Y],L1),create_path(Y,L1). 
+
+validPaths(_):- start(K),start_path(K),fail.
+
+start_path([H|T]):- path(H,[H]),fail.
+start_path([_|T]):- start_path(T),fail.
+
+path(X,L):- X=='g17',write("Path: "),write(L),nl,!,fail.
+% path(X,L):- restr(X),gates(G),edges(X,G,E1),dfs(E1,L,1),fail.
+path(X,L):- gates(G),edges(X,G,E1),dfs(E1,L,0),fail.
+
+dfs([],_,_).
+% dfs([H|T],L,1):-H \='na',append(L,[H],L1),path(H,L1),fail.
+dfs([H|T],L,0):- H \='na',not(member(H,L)),append(L,[H],L1),path(H,L1),fail.
+dfs([H|T],L,Parent):- dfs(T,L,Parent),fail.
+
+
+a(X):- gates(T),edges(X,T,R1),write(R1).
